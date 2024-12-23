a1490aa23e5bf6dc8b355ce6be55b132697674ef
Atharvashirsh Tiwary <121449422+Atharvashirsh@users.noreply.github.com>
2024-12-23 23:36:57 +0530

    feat: backend project setup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
+const appRoutes = require("./routes/router");
+const dotenv = require("dotenv");
 const express = require("express");
 
+dotenv.config();
 const app = express();
 
-app.listen(3000, () => {
+app.use("/api", appRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log("Server running on port 3000");
 });
